e52ab876c9e12a869840a4c0fd4bd6f98ad08fdf
Lucas Luo <98566376+LWZsama@users.noreply.github.com>
2026-02-12 22:34:21 +0000

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/eep-assembler-web/",
   resolve: {
     alias: {
       "fable-library-js": "@fable-org/fable-library-js",
